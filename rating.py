151a7aeaac977f405346789b22dfb4f86b5e40fe
Sirui Feng <siruif@cs.uchicago.edu>
2016-02-21 17:04:36 -0600

    modified rating.py

--- rating.py
+++ rating.py
@@ -1,55 +1,68 @@
 import sqlite3
 import math
+import csv
 connection = sqlite3.connect("EducationData.db")
 cursor = connection.cursor()
 
 
-s2 = "SELECT g.CPSUnit, p.SQRPRating, e.Expenditures, l.Total \
+s2 = "SELECT g.CPSUnit, p.SQRPRating, sum(e.Expenditures) as totalexpend, l.Total, g.FullName,  g.Latitude, g.longitude \
 FROM general AS g JOIN expenditure AS e ON g.CPSUnit = e.CPSUnit \
-JOIN lunch AS l ON l.CPSUnit = g.CPSUnit JOIN performance AS p ON p.CPSUnit = g.CPSUnit;"
+JOIN lunch AS l ON l.CPSUnit = g.CPSUnit JOIN performance AS p ON p.CPSUnit = g.CPSUnit \
+GROUP BY g.FullName;"
 #s2 is for heat map
 
 rating_data = cursor.execute(s2)
+
 
 UNIT = 0
 RATING = 1
 EXPEND = 2
 TOTAL = 3
-rate_criteria={"Level 1+": 90, "Level 1":70, "Level 2+": 50, "Level 2": 40, "Level 3": 20, "Inability to Rate": None}
+NAME = 4
+LAT = 5
+LON = 6
+rate_criteria = {"Level 1+": 90, "Level 1":70, "Level 2+": 50, "Level 2": 40, "Level 3": 20, "Inability to Rate": None}
 #reference: http://cps.edu/SiteCollectionDocuments/SQRP_one_pager.pdf
 #for Level 3: assign 20
 
-rating_dict = {}
+rating_list = []
+
 for each in rating_data:
-	key = each[UNIT]
-	if key not in rating_dict:
-		rating_dict[key]={}
-		rating = each[RATING]
-		score = rate_criteria[rating]
+	full_name = each[NAME]
+	rating = each[RATING]
+	score = rate_criteria[rating]
 
-		expend = each[EXPEND]
-		expend = expend.replace(",","")
-		expend = float(expend)
+	expend = each[EXPEND]
+	expend = float(expend)
 
-		total = each[TOTAL]
-		total = total.replace(",","")
-		total = float(total)
+	total = each[TOTAL]
+	total = total.replace(",","")
+	total = float(total)
 
-		rating_dict[key]["rating"] = rate_criteria[rating]
-		rating_dict[key]["expenditure"] = expend
-		rating_dict[key]["total"] = total
+	school=[]
+	school.append(full_name)
+	school.append(each[LON])
+	school.append(each[LAT])
 
-		print("the type of score is", type(score))
-		print("the type of expend is", type(expend))
-		print("the type of total is", type(total))
-		print("the total is", total)
-		print("the expenditure is:", expend)
-		if score!=None:
-			if expend >0:
-			
-				rating_dict[key]["scoreperexpend"] = math.log(score / (expend/total))
-		else:
-			rating_dict[key]["scoreperexpend"] = None
-print(rating_dict)
+	if score!=None:
+		if expend >0:
+			score = round(math.log(score / (expend/total)),2)
+	else:
+		score = None
+	school.append(score)
+	rating_list.append(school)
+
+for each in rating_list:
+	print(each)
+
+
+with open('scores.csv', 'w') as outcsv:   
+    #configure writer to write standard csv file
+    writer = csv.writer(outcsv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+    writer.writerow(['full_name', 'lon', 'lat', 'score'])
+    for item in rating_list:
+        #Write item to outcsv
+        writer.writerow([item[0], item[1], item[2], item[3]])
+
 
 connection.close()
