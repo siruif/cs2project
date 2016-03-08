d7100c46dac34d4b619113e87bddce418d305d02
Vi Nguyen <vi.nguyen@aya.yale.edu>
2016-03-07 19:51:07 -0600

    some edits to form

--- mysite/edmoneyball/forms.py
+++ mysite/edmoneyball/forms.py
@@ -5,9 +5,9 @@ DEFAULT_CHOICE  = [""]
 
 SCHOOL_CHOICES = school_info.school_names()
 
-RACE_CHOICES = ['Asian','Black', 'White', 'Hispanic', 'Multi Racial']
+RACE_CHOICES = ['Asian','Black', 'White', 'Hispanic', 'Multiracial']
 
-SCHOOL_TYPES = ['Charter', 'Neighbourhood']
+SCHOOL_TYPES = ['Charter', 'Neighborhood']
 
 class AddressForm(forms.Form):
     
@@ -15,19 +15,19 @@ class AddressForm(forms.Form):
 
 class ReccomendationForm(forms.Form):
     
-    performance = forms.ChoiceField ( label = 'Performance',choices = [(x, x) for x in range ( 0, 105, 5 ) ] )
+    performance = forms.ChoiceField ( label = 'Performance \n',choices = [(x, x) for x in range ( 0, 105, 5 ) ] )
     
-    free_red_lunch = forms.ChoiceField( label = 'Free and Reduced Lunch',choices = [ ( x, x ) for x in range ( 0, 105, 5 ) ] )
+    free_red_lunch = forms.ChoiceField( label = 'Free and Reduced Lunch \n',choices = [ ( x, x ) for x in range ( 0, 105, 5 ) ] )
     
-    ethnicity = forms.ChoiceField ( label = 'Ethnicity', choices = [ ( x, x ) for x in DEFAULT_CHOICE + RACE_CHOICES ]  )
+    ethnicity = forms.ChoiceField ( label = 'Ethnicity \n', choices = [ ( x, x ) for x in DEFAULT_CHOICE + RACE_CHOICES ]  )
 
-    ethnicity_threshold = forms.ChoiceField ( label = 'Ethnicity Threshold',choices = [ ( x, x ) for x in range ( 0, 105, 5 ) ] )
+    ethnicity_threshold = forms.ChoiceField ( label = 'Ethnicity Threshold \n',choices = [ ( x, x ) for x in range ( 0, 105, 5 ) ] )
     
-    school_type = forms.ChoiceField ( label = 'Type of School',choices = [ ( x, x ) for x in DEFAULT_CHOICE + SCHOOL_TYPES ], required = False )
+    school_type = forms.ChoiceField ( label = 'Type of School \n',choices = [ ( x, x ) for x in DEFAULT_CHOICE + SCHOOL_TYPES ], required = False )
 
-    location = forms.CharField( label = 'Please Enter Your Address', required = False )
+    location = forms.CharField( label = 'Please Enter Your Address\n', required = False )
 
-    distance_threshold = forms.FloatField ( label = 'Find Schoold within miles', required = False  )
+    distance_threshold = forms.FloatField ( label = 'Maximum Distance From Address (Miles) \n', required = False  )
 
 class ComparisonForm(forms.Form):
     
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'edmoneyball'
+    'bootstrapform',
+    'edmoneyball',
 )
 
 MIDDLEWARE_CLASSES = (
