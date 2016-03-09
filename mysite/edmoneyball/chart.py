35b30a51706ebc4a4fc56fc66d7e600c41de2118
Vi Nguyen <vi.nguyen@aya.yale.edu>
2016-03-09 15:19:04 -0600

    added doc strings

--- mysite/edmoneyball/chart.py
+++ mysite/edmoneyball/chart.py
@@ -1,23 +1,28 @@
+# CS 122 Project: EdMoneyBall
+# Vi Nguyen, Sirui Feng, Turab Hassan
+
 from . import school_info
-#import school_info
 import matplotlib.pylab as plt
 import numpy as np
 import plotly.plotly as py
 import plotly.tools as tls 
 import plotly.graph_objs as go
-
+## commented out to align with Django
+#import school_info
 #from plotly.graph_objs import *
 
+
+# Sign-in accounts in case hit the 50/chart daily limit on plotly
+# i.e. when request is "throtteled"
 py.sign_in('vi-tnguyen', '2j59j4yh6y')
 #py.sign_in('siruif', '1xbbym8vxv')
 #py.sign_in('nvi613', 'dceant1x53')
-<<<<<<< Updated upstream
-py.sign_in('turabhassan', 'qu73c973p4')
-=======
 #py.sign_in('turabhassan', 'qu73c973p4')
->>>>>>> Stashed changes
+#py.sign_in('turabhassan', 'qu73c973p4')
 #py.sign_in('sarahfsr', 'g263mhd6au')
-# Key sets and dictionaries for dealing with expenditure data
+
+
+# Key sets and dictionaries for processing and cleaning expenditure data
 Expenditure_Cat = set(['Admin Salary & Benefits', \
                   'Operational Expenses', 'Teacher Salary & Benefits',\
                   'Pensions', 'Capital Expenses', 
@@ -27,7 +32,7 @@ Expenditure_Cat_Rename = {'Operational Expenses': 'Operations', \
                           'Capital Expenses':'Capital', \
                           'Instructional-Related Expenses': 'Instructional-Related'}
 
-# Key sets and dictionaries for dealing with ethnicity data
+# Key sets and dictionaries for processing and cleaning ethnicity data
 ethnicity_cat = set(['asian', 'white', 'african', 'hispanic', 'multi'])
 ethnicity_cat_rename = {'asian': 'Asian', \
                         'white': 'Caucasian', \
@@ -35,20 +40,20 @@ ethnicity_cat_rename = {'asian': 'Asian', \
                         'hispanic':' Hispanic',\
                         'multi': 'Multi-racial'}
 
-# Key sets and dictionaries for dealing with free, reduced lunch data
+# Key sets and dictionaries for processing and cleaning free, reduced lunch data
 frlunch_cat = set(['free_red_lunch'])
 frlunch_cat_rename = {'free_red_lunch': 'Free/Reduced Lunch'}
 
-# Key sets and dictionaries for dealing with academic performance data
+# Key sets and dictionaries for processing and cleaning academic performance data
 acad_perf_cat = set(['rdg_growth', 'math_growth'])
-acad_perf_cat_rename = {'rdg_growth': 'Growth in Reading', 'math_growth': 'Growth in Math'}
-
+acad_perf_cat_rename = {'rdg_growth': 'Growth in Reading', 'math_growth': \
+                        'Growth in Math'}
 
 # Pulls in data dictionary of school data and creates a list of 
 # variables for which we are bringing in data
 district_data = school_info.create_school_dictionary()
 
-# Pulling in 'total students' to calculate expenditure per student
+# Pulls in 'total students' to calculate expenditure per student
 students = set(['total_students'])
 
 # Pulls in 'total_expenditures' to get district avg
@@ -60,28 +65,34 @@ variables = list(district_data['Wolfgang A Mozart Elementary School'].keys())
 variables_charts = frlunch_cat | Expenditure_Cat | ethnicity_cat | \
                    Expenditure_Cat_Unknown | students | total_expend | acad_perf_cat
 
-# List of variables that will not be a number, to be used to deal with type incongruencies
-non_num_var = set(['type', 'unit', 'perf_rating', 'address', 'attending_grades' ])
-
-#print(district_data)
-
+# List of variables that will not be a number, to be used to deal with type 
+# incongruencies
+non_num_var = set(['type', 'unit', 'perf_rating', 'address', 'attending_grades'])
 
 # Deal with type incongruencies
 for school in district_data.keys():
     for var in district_data[school]:
-        if var not in non_num_var: # Exclude processing of variables for which our data is not a number
+        # Exclude processing of variables for which our data is not a number
+        if var not in non_num_var:
             data_point = district_data[school][var]
             if type(data_point) is str:
-                #print('string loop')
                 data_point = data_point.replace(',', '')
                 data_point = data_point.strip('%')
                 data_point = float(data_point)
                 district_data[school][var] = data_point
-            #print('data_point: {}, type: {}'.format(data_point, type(data_point)))
-#print(district_data)
 
 # Calculate district averages for baseline in charts
 def district_avg():
+    '''
+    Calcultates the district average for the expenditure, performance, and
+    diversity measures based on the data dictionary of district data for the
+    elementary schools that we called and processed above. 
+
+    Returns a dictionary with 'district_avg' as the key and corresponding values 
+    for each measure of expenditure category, performance growth levels for math 
+    and reading, the number of students on free-and-reduced lunch, and the 
+    average per ethnicity
+    '''
     district_avg = {}
     district_dict = {}
     for var in variables:
@@ -96,13 +107,28 @@ def district_avg():
                     avg = total / len(district_data)
             district_avg[var] = avg
     district_dict['district_avg'] = district_avg
-    #print(district_dict)
 
-    #print('testing')
     return district_dict
 
 
-def create_labels_values(school_name, data_dictionary, data_labels, renamed_labels = {}):
+def create_labels_values(school_name, data_dictionary, data_labels, \
+    renamed_labels):
+    '''
+    Creates a list of labels, and a corresponding list of values for plotting
+
+    Inputs: 
+        -school_name - school name, 
+        -data_dictionary - dictionary of key data elements for elementary 
+            schools in Chicago
+        -data_labels - list of data elements we want to bring in
+        -renamed_labels - dictionary of how we want the data elements to be \
+            renamed for displaying (renamed_labels)--
+    Outputs:
+        labels - a list of labels, 
+        values - a list of values for the charts, 
+        school_data - dictionary with the school name as the key and dictionaries \
+        for each data element we're bringing in.
+    '''
     school_data = data_dictionary[school_name]
     labels = []
     values = []
@@ -124,38 +150,57 @@ def create_labels_values(school_name, data_dictionary, data_labels, renamed_labe
     return labels, values, school_data
 
 
-# Create expenditure pie charts
-def expenditure_pie(school_data, school_name, labels_school, values_school, labels_distr, values_distr):
-    # Creating chart
+def expenditure_pie(school_data, school_name, labels_school, values_school, \
+                    labels_distr, values_distr):
+    '''
+    Charts the expenditure donut charts 
+    Inputs:
+        school_data - a data dictionary with a as the key and dictionaries for 
+            each data element we're bringing
+        school_name - name of a school
+        labels_school - list of labels that go with the school data
+        values_school - corresponding list of values that go with the school data
+        labels_distr - list of labels that go with the district average data
+        values_distr - corresponding list of values that go with the district 
+            average data
+    Outputs:
+        a url where plotly is charting the donot charts of expenditure for the
+            school, and district average (for comparison purposes)
+    '''
     title = 'Total Expenditures Per Student: \n ${0}' .format("{:,.0f}".\
         format(school_data[school]['total_expend']))
-    #fig = {'data': [{'labels': labels, 'values': values, 'type': 'pie', 'domain': {'x': [0, .48]}, \
-    #'hole': .4}], 'layout': {'title': title}}
-
-    fig = {"data": [{"values": values_school, "labels": labels_school, "domain": {"x": [0, .48]},\
-            "name": school_name, "hoverinfo":"label+percent", "hole": .4, "type": "pie"}, \
-            {"values": values_distr, "labels": labels_distr, "textposition":"inside",\
-            "domain": {"x": [.52, 1]},"name": "District Average", "hole": .4, "type": "pie"}],\
-            "layout": {"title": title, \
-            "annotations": [{"font": {"size": 10}, "showarrow": False, 'text': 'School', "x": 0.20, \
-            "y": 0.5}, {"font": {"size": 10}, "showarrow": False, "text": 'District', "x": 0.8, "y": 0.5}]}}
+    fig = {"data": [{"values": values_school, "labels": labels_school, "domain":\
+        {"x": [0, .48]},"name": school_name, "hoverinfo":"label+percent", "hole":\
+        .4, "type": "pie"}, {"values": values_distr, "labels": labels_distr,\
+        "textposition":"inside", "domain": {"x": [.52, 1]},"name": "District Average",\
+        "hole": .4, "type": "pie"}], "layout": {"title": title, "annotations":\
+         [{"font": {"size": 10}, "showarrow": False, 'text': 'School', "x": 0.20,\
+         "y": 0.5}, {"font": {"size": 10}, "showarrow": False, "text": 'District',\
+         "x": 0.8, "y": 0.5}]}}
 
     url = py.plot(fig, filename='Pie Chart: Expenditure', auto_open = False)
-    #py.image.save_as(fig, 'charts/expenditure_pie.png')
 
     return url
 
 
 def expenditure_data(school_name, data_dictionary):
-
-    labels, values, school_data = create_labels_values(school_name, data_dictionary, Expenditure_Cat, \
-                     renamed_labels = Expenditure_Cat_Rename)
+    '''
+    Special function to process expenditure data so we can show it as 
+    expenditure per student
+    Inputs:
+        school_name - name of the school that we're processing
+        data_dictionary - dictionary of data for all elementary schools in the
+            Chicago Public Schools
+    Outputs:
+        labels - labels to use for plotting
+        values - values to use for plotting
+    '''
+    labels, values, school_data = create_labels_values(school_name, data_dictionary,\
+        Expenditure_Cat, renamed_labels = Expenditure_Cat_Rename)
 
     # converting values to numpy arrays to get spend per student
     values = np.array(values)
-    #print(values)
     students = school_data['total_students']
-    #print(type(students))
     if type(students) is str:
         students = students.replace(',', '')
     values = values / float(students)
@@ -173,19 +218,34 @@ def expenditure_data(school_name, data_dictionary):
     return labels, values
 
 
-def bar(school_name, data_dictionary, data_distr_avg, cat_dict, cat_dict_rename, chart_title):
-    labels_school, values_school, school_data = create_labels_values(school_name, data_dictionary, cat_dict,\
-                                    renamed_labels = cat_dict_rename)
-
-    labels_distr, values_distr, district_data = create_labels_values('district_avg', data_distr_avg, \
-                                                cat_dict, renamed_labels = cat_dict_rename)
-    #print(labels_school, values_school)
+def bar(school_name, data_dictionary, data_distr_avg, cat_dict, cat_dict_rename, \
+    chart_title):
+    '''
+    Create bar charts for each school that is passed through, and 
+    a chart of the district average for comparison purposes
+    Inputs:
+        school_name - name of school
+        data_dictionary - dictionary of data for all elementary schools in the
+            Chicago Public Schools
+        data_distr_avg - data dictionary with 'district_avg' as key and average
+            values for key data elements for the district
+        cat_dict - dictionary of data elements we're using to build the bar plot
+        cat_dict_rename - dictionary with each category as the keys and the renamed
+            values 
+        chart_title - title we want for the chart
+    Outputs:
+        -url of where plotly charted the graph
+    '''
+    
+    labels_school, values_school, school_data = create_labels_values(school_name, \
+        data_dictionary, cat_dict, renamed_labels = cat_dict_rename)
     trace1 = go.Bar(x = labels_school, y = values_school, name = 'School')
 
+    labels_distr, values_distr, district_data = create_labels_values('district_avg', \
+        data_distr_avg, cat_dict, renamed_labels = cat_dict_rename)
     trace2 = go.Bar(x = labels_distr, y = values_distr, name = 'District Average')
 
     students = school_data['total_students']
-
     title = '{0}: {1} \n Total Students: {2}' .format(chart_title, school_name, 
         "{:,.0f}".format(float(students)))
     layout = go.Layout(yaxis = dict(title = 'Percentage (%)'), \
@@ -193,15 +253,26 @@ def bar(school_name, data_dictionary, data_distr_avg, cat_dict, cat_dict_rename,
 
     data = [trace1, trace2]
     fig = go.Figure(data = data, layout = layout)
-
-    url = py.plot(fig, filename = 'Bar Chart: {}'.format(chart_title), auto_open = False)
-    #py.image.save_as(fig, 'charts/demographic_bar.png')
+    url = py.plot(fig, filename = 'Bar Chart: {}'.format(chart_title), \
+        auto_open = False)
 
     return url
 
 
 def frlunch_bar(school_name, data_dictionary, data_distr_avg):
-    labels, values, school_data = create_labels_values(school_name, data_dictionary, \
+    '''
+    Create bar charts free_lunch data for each school that is passed through, and 
+    a chart of the district average for comparison purposes
+    Inputs:
+        school_name - name of school
+        data_dictionary - dictionary of data for all elementary schools in the
+            Chicago Public Schools
+        data_distr_avg - data dictionary with 'district_avg' as key and average
+            values for key data elements for the district
+    Outputs:
+        -url of where plotly charted the graph    
+    '''
+    labels, values, school_data = create_labels_values(school_name, data_dictionary,\
         frlunch_cat, frlunch_cat_rename)
     on_frlunch = values[0]        
     labels = ['School']
@@ -215,40 +286,72 @@ def frlunch_bar(school_name, data_dictionary, data_distr_avg):
 
     fig = go.Figure(data = data, layout = layout)
     url = py.plot(fig, filename = 'Bar Chart: Income Indicator', auto_open = False)
-    #py.image.save_as(fig, filename = 'charts/frlunch_pie.png')
 
     return url
 
-def bar_compare(school, cat_dict, cat_dict_rename, chart_title, data_dictionary, data_distr_avg):
+
+def bar_compare(school_name, cat_dict, cat_dict_rename, chart_title, data_dictionary,\
+    data_distr_avg):
+    '''
+    Create bar chart data for each school that we're comparing to in the compare
+    function
+    Inputs:
+        school_name - name of school
+        cat_dict - dictionary of data elements we're using to build the bar plot
+        cat_dict_rename - dictionary with each category as the keys and the renamed
+            values 
+        chart_title - title we want for the chart
+        data_dictionary - dictionary of data for all elementary schools in the
+            Chicago Public Schools
+        data_distr_avg - data dictionary with 'district_avg' as key and average
+            values for key data elements for the district
+    Outputs:
+        -bar chart data that plotly can call on to chart    
+    '''
     data_dictionary['District Average*'] = data_distr_avg['district_avg']
     if chart_title == 'Expenditures Per Student':
         labels, values = expenditure_data(school, data_dictionary)
         print(labels, values)
     else:
-        labels, values, school_data = create_labels_values(school, data_dictionary, \
+        labels, values, school_data = create_labels_values(school_name, data_dictionary,\
                                       cat_dict, cat_dict_rename)
-    data = go.Bar(x = labels, y = values, name = school)
+    data = go.Bar(x = labels, y = values, name = school_name)
+
     return data
 
-def compare(list_of_schools, cat_dict, cat_dict_rename, chart_title, data_dictionary, data_distr_avg):
+
+def compare(list_of_schools, cat_dict, cat_dict_rename, chart_title, data_dictionary,\
+     data_distr_avg):
+    '''
+    Generates charts based on user input to compare schools that are passed through
+    Inputs:
+        list_of_schools - names of school passed to us through user interface
+        cat_dict - dictionary of data elements we're using to build the bar plot
+        cat_dict_rename - dictionary with each category as the keys and the renamed
+            values 
+        chart_title - title we want for the chart
+        data_dictionary - dictionary of data for all elementary schools in the
+            Chicago Public Schools
+        data_distr_avg - data dictionary with 'district_avg' as key and average
+            values for key data elements for the district
+    Outputs:
+        -url where plotly charted the graph
+    '''
     list_traces = []
-    #print(list_of_schools)
     if 'District Average*' not in list_of_schools:
         list_of_schools.append('District Average*')
     for school in list_of_schools:
-        #print(school)
-        trace1 = bar_compare(school, cat_dict, cat_dict_rename,chart_title, data_dictionary, \
-                            data_distr_avg)
+        trace1 = bar_compare(school_name, cat_dict, cat_dict_rename,chart_title, \
+            data_dictionary, data_distr_avg)
         list_traces.append(trace1)
-        #print(list_traces)
 
     data = list_traces
     layout = go.Layout(barmode = 'group', title = chart_title)
     
     fig = go.Figure(data = data, layout = layout)
-    url = py.plot(fig, filename = 'grouped-bar {}'.format(chart_title), auto_open = False)
+    url = py.plot(fig, filename = 'grouped-bar {}'.format(chart_title), \
+        auto_open = False)
     
     return url
-    #py.image.save_as(fig, filename = 'charts/compare2.png')
 
 
--- mysite/edmoneyball/update_charts.py
+++ mysite/edmoneyball/update_charts.py
@@ -1,57 +1,38 @@
 
 
 from . import school_info, chart, ranking, school_zone
+## commented out to align with Django
 #import chart
 #import ranking
 #import school_info
 
+# calling dictionaries to increase speed
 data = school_info.create_school_dictionary()
 data_distr_avg = chart.district_avg()
 
-'''
-school =  'Jane A Neil Elementary School'
-school =  'Woodlawn Community Elementary School'
-
-data = school_info.create_school_dictionary()
-
-#return
-data_distr_avg = chart.district_avg()
-#print(data[school]['total_expend'])
-#print(data_distr_avg['district_avg']['total_expend'])
-schools = list(data.keys())
-list_of_schools1 = schools[0:5]
-
-#print(chart.frlunch_bar(school, data, data_distr_avg))
-
-print(chart.bar(school, data, data_distr_avg, chart.ethnicity_cat, chart.ethnicity_cat_rename,\
-                            'Ethnicity'))
-
-#print(chart.bar(school, data, data_distr_avg, chart.acad_perf_cat, chart.acad_perf_cat_rename,\
-#                            'Academic Performance'))
-
-#labels_school, values_school = chart.expenditure_data(school, data)
-#print('school; label, values:', labels_school, values_school)
-#labels_distr, values_distr = chart.expenditure_data('district_avg', data_distr_avg)
-#print('school: label, values:', labels_distr, values_distr)
-#print(chart.expenditure_pie(data, school, labels_school, values_school, labels_distr, values_distr))
-#print(chart.district_avg())
-'''
-
-## Functions for Django
 
 def create_charts(school):
-
-    # Run charts with specified school
+    '''
+    Given a school name, returns a list of urls where we plot the school's data 
+    and the district average for:
+        -Percent of students on free and reduced Lunch
+        -Distribution of ethnicity groups among students
+        -Math/Reading growth scores, and grade level scores
+        -Expenditures
+    '''
+    # Income indicator
     urls = {'school': school}
     url_frl = chart.frlunch_bar(school, data, data_distr_avg)
     urls['url1'] = url_frl
 
-    url_eth = chart.bar(school, data, data_distr_avg, chart.ethnicity_cat, chart.ethnicity_cat_rename,\
-                                'Ethnicity')
+    # Ethnicity
+    url_eth = chart.bar(school, data, data_distr_avg, chart.ethnicity_cat, \
+        chart.ethnicity_cat_rename, 'Ethnicity')
     urls['url2'] = url_eth
 
-    url_perf = chart.bar(school, data, data_distr_avg, chart.acad_perf_cat, chart.acad_perf_cat_rename,\
-                                'Academic Performance')
+    # Performance
+    url_perf = chart.bar(school, data, data_distr_avg, chart.acad_perf_cat, \
+        chart.acad_perf_cat_rename, 'Academic Performance')
     urls['url3'] = url_perf
 
     # Expenditure chart
@@ -60,16 +41,21 @@ def create_charts(school):
     url_exp = chart.expenditure_pie(data, school, labels_school, values_school, labels_distr, values_distr)
     urls['url4'] = url_exp
 
-    #print(urls)
     return urls
 
 
-#pref_crit_from_ui = {'special_educ': 55.0, 'performance': 88.0, 'distance_threshold': 66.0, \
-#    'location': [41.9449905,-87.6843248], 'free_red_lunch': 56.0, 'type': 'charter','ethnicity': 'asian', \
-#    'ethnicity_threshold': 20.0}
-
-
-def compare_recommend(recommend_indicator,pref_crit_from_ui = None, list_of_schools = None):
+def compare_recommend(recommend_indicator, pref_crit_from_ui = None, \
+    list_of_schools = None):
+    '''
+    Plots the graphs for either the schools that the user wants to compare, 
+    or the top 5 schools that we recommend to the user
+    Inputs:
+        recommend_indicator - True or False to indicate whether this function 
+            is being called to chart the recommended schools (True) or just to
+            chart the comparisons
+    Outputs:
+        list of urls where plotly charted the graphs
+    '''
 
     if recommend_indicator:
         clean_data = ranking.clean_data(pref_crit_from_ui)
@@ -77,17 +63,22 @@ def compare_recommend(recommend_indicator,pref_crit_from_ui = None, list_of_scho
 
     urls = {'school': list_of_schools}
 
-    url1 = chart.compare(list_of_schools, chart.Expenditure_Cat, chart.Expenditure_Cat_Rename,\
-                     'Expenditures Per Student', data, data_distr_avg)
+    url1 = chart.compare(list_of_schools, chart.Expenditure_Cat, \
+        chart.Expenditure_Cat_Rename, 'Expenditures Per Student', data, \
+        data_distr_avg)
     urls['url1'] = url1
-    url2 = chart.compare(list_of_schools, chart.ethnicity_cat, chart.ethnicity_cat_rename,\
-                     'Ethnicity', data, data_distr_avg)
+
+    url2 = chart.compare(list_of_schools, chart.ethnicity_cat, \
+        chart.ethnicity_cat_rename, 'Ethnicity', data, data_distr_avg)
     urls['url2'] = url2
-    url3 = chart.compare(list_of_schools, chart.frlunch_cat, chart.frlunch_cat_rename,\
-                     'Income Indicator: Free and Reduced Lunch', data, data_distr_avg)
+    
+    url3 = chart.compare(list_of_schools, chart.frlunch_cat, \
+        chart.frlunch_cat_rename, 'Income Indicator: Free and Reduced Lunch', \
+        data, data_distr_avg)
     urls['url3'] = url3
-    url4 = chart.compare(list_of_schools, chart.acad_perf_cat, chart.acad_perf_cat_rename,\
-                     'Academic Performance', data, data_distr_avg)
+
+    url4 = chart.compare(list_of_schools, chart.acad_perf_cat, \
+        chart.acad_perf_cat_rename, 'Academic Performance', data, data_distr_avg)
     urls['url4'] = url4
 
     return urls
