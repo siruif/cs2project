bc08fbed624c120c55b5a0e7e35fa41b1f6a1574
Vi Nguyen <vi.nguyen@aya.yale.edu>
2016-03-13 13:12:06 -0500

    routed localhost to index page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from edmoneyball import views
 
 urlpatterns = [
     url(r'^edmoneyball/', include('edmoneyball.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name = 'index')
 ]
